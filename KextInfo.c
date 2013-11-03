68bec28ed53142867ffc271c5dc9535f75549d02
winocm <winocm@icloud.com>
2013-11-03 12:17:56 -0600

    Fix an issue with the example.

--- KextInfo.c
+++ KextInfo.c
@@ -7,7 +7,7 @@
 extern kern_return_t _start(kmod_info_t *ki, void *data);
 extern kern_return_t _stop(kmod_info_t *ki, void *data);
  
-__attribute__((visibility("default"))) KMOD_EXPLICIT_DECL(KEXT_BUNDLE_FULL, KEXT_VERSION, _start, _stop)
+__attribute__((visibility("default"))) KMOD_EXPLICIT_DECL(com.apple.driver.AppleTester, KEXT_VERSION, _start, _stop)
 __private_extern__ kmod_start_func_t *_realmain = 0;
 __private_extern__ kmod_stop_func_t *_antimain = 0;
 __private_extern__ int _kext_apple_cc = __APPLE_CC__ ;
